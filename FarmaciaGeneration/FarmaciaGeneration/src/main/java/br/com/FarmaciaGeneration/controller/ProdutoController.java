158616fd8e74517a38552d586affdca9053d5176
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 14:42:08 -0300

    deleteProduto

--- FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
+++ FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.FarmaciaGeneration.model.Categoria;
 import br.com.FarmaciaGeneration.model.Produto;
 import br.com.FarmaciaGeneration.repository.ProdutoRepository;
 
@@ -48,12 +48,9 @@ public class ProdutoController {
 	public ResponseEntity<Produto> put (@RequestBody Produto produto){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
 }
