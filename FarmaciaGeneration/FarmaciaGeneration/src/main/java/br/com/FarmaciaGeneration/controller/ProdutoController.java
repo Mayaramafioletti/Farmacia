ef7fac3ca913929ba17e7554b43e0cb533550836
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 14:39:58 -0300

    postProduto

--- FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
+++ FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
@@ -3,10 +3,13 @@ package br.com.FarmaciaGeneration.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +39,10 @@ public class ProdutoController {
 	public ResponseEntity<List<Produto>> getByName(@PathVariable String nome){
 		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
 	}
-	
+	@PostMapping
+	public ResponseEntity<Produto> post (@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
 	
 	
 	
