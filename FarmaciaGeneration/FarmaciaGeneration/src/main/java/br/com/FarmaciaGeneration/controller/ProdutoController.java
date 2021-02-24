163b98e038d7ae8b8fe1c5bf7417e7ef5122472f
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 14:30:46 -0300

    @Autowired

--- FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
+++ FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
@@ -1,12 +1,19 @@
 package br.com.FarmaciaGeneration.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.FarmaciaGeneration.repository.ProdutoRepository;
+
+
 
 @RestController
 @RequestMapping("/produto")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
-
+	@Autowired
+	private ProdutoRepository repository;
+	
 }
