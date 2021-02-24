72487905aa5b658b6c8ffa179658f77173e18eff
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 14:33:08 -0300

    findById

--- FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
+++ FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/ProdutoController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.com.FarmaciaGeneration.model.Produto;
 import br.com.FarmaciaGeneration.repository.ProdutoRepository;
@@ -23,5 +25,11 @@ public class ProdutoController {
 	@GetMapping
 	public ResponseEntity<List<Produto>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
