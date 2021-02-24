c9f0ea40f831614c6e2e4b4d2185cd3fc8260f27
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 14:08:14 -0300

    findAll

--- FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/CategoriaController.java
+++ FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/controller/CategoriaController.java
@@ -1,10 +1,15 @@
 package br.com.FarmaciaGeneration.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.FarmaciaGeneration.model.Categoria;
 import br.com.FarmaciaGeneration.repository.CategoriaRepository;
 
 
@@ -14,5 +19,8 @@ import br.com.FarmaciaGeneration.repository.CategoriaRepository;
 public class CategoriaController {
 	@Autowired
 	private CategoriaRepository repository;
-	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
 }
