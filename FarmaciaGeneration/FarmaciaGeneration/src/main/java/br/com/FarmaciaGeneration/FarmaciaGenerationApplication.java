bfa1d03878f534eabd16596a2e0b64b35df6b02e
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 13:04:18 -0300

    application.properties

---
+++ FarmaciaGeneration/FarmaciaGeneration/src/main/java/br/com/FarmaciaGeneration/FarmaciaGenerationApplication.java
@@ -0,0 +1,13 @@
+package br.com.FarmaciaGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaGenerationApplication.class, args);
+	}
+
+}
---
+++ FarmaciaGeneration/FarmaciaGeneration/src/test/java/br/com/FarmaciaGeneration/FarmaciaGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.FarmaciaGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
